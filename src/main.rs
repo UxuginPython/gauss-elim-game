8a06c134bec38a78082add8107162e76dc895c43
Uxugin <git@uxugin.com>
2025-04-18 19:33:46 -0600

    start math stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,45 @@
 use gtk4::prelude::*;
 use gtk4::{Application, ApplicationWindow, DrawingArea, Orientation, glib};
+const SYSTEM_SIZE: usize = 4;
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Equation {
+    coefficients: [f64; SYSTEM_SIZE],
+    solution: f64,
+}
+impl Equation {
+    const fn new(coefficients: [f64; SYSTEM_SIZE], solution: f64) -> Self {
+        Self {
+            coefficients: coefficients,
+            solution: solution,
+        }
+    }
+}
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct System {
+    equations: [Equation; SYSTEM_SIZE],
+}
+impl System {
+    const fn new(equations: [Equation; SYSTEM_SIZE]) -> Self {
+        Self {
+            equations: equations,
+        }
+    }
+    const fn switch_rows(&mut self, a: usize, b: usize) {
+        let row_a = self.equations[a];
+        let row_b = self.equations[b];
+        self.equations[b] = row_a;
+        self.equations[a] = row_b;
+    }
+}
+#[rustfmt::skip]
+static mut SYSTEM: System = System::new([
+    Equation::new([1.0, 0.0, 0.0, 0.0], 1.0),
+    Equation::new([0.0, 1.0, 0.0, 0.0], 2.0),
+    Equation::new([0.0, 0.0, 1.0, 0.0], 3.0),
+    Equation::new([0.0, 0.0, 0.0, 1.0], 4.0),
+]);
 fn main() -> glib::ExitCode {
+    println!("{:#?}", unsafe { SYSTEM });
     let app = Application::builder()
         .application_id("com.uxugin.matrixfun")
         .build();
